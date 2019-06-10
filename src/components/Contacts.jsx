d7990e2563202202129c63c82f379b116a8dd056
Alex Tkachuk <alex@homecareheroes.com.au>
2019-06-11 09:58:00 +1000

    Change Contacts

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -4,10 +4,15 @@ function Contacts(props) {
   return (
     <div id="contact" className="container">
       <h3 className="text-center">{props.data.header}</h3>
-      <p className="text-center">{props.data.text}</p>
+      <p className="text-center wrap-text">{props.data.text}</p>
       <div className="row test">
         <div className="col-md-12">
           <p className="text-center"><span className="glyphicon glyphicon-envelope" />{props.data.mail}</p>
+        </div>
+      </div>
+      <div className="row test">
+        <div className="col-md-12">
+          <p className="text-center"><a rel="noopener noreferrer" target="_blank" style={{ color: 'black' }} href={`https://${props.data.link}`}>{props.data.link}</a></p>
         </div>
       </div>
     </div>
@@ -15,6 +20,7 @@ function Contacts(props) {
 }
 
 Contacts.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
   data: PropTypes.object.isRequired,
 };
 export default Contacts;
